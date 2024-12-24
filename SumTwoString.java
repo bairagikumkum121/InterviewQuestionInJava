f9b37c78f33c7e70d5a01665050570d4352fb842
kumkum Bairagi <127207925+bairagikumkum121@users.noreply.github.com>
2024-12-24 10:11:16 +0530

    SumTwoString

---
+++ SumTwoString.java
@@ -0,0 +1,12 @@
+package com.string.question;
+
+public class SumTwoString {
+	public static void main(String[] args) {
+		String str1 = "111";
+		String str2 = "222";
+		int sum = Integer.parseInt(str1) + Integer.parseInt(str2);
+		System.out.println("Addition of string is: " + sum);
+	}
+}
+// output-
+// Addition of string is: 333
